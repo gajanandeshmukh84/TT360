09626698c12eca7494011809ff2de94e45028435
Prateek <prateemsingh4736@gmail.com>
2026-03-06 12:42:55 +0530

    Add Spring Boot project setup

---
+++ TT360-app/src/main/java/org/team/tt360app/Tt360AppApplication.java
@@ -0,0 +1,13 @@
+package org.team.tt360app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tt360AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tt360AppApplication.class, args);
+    }
+
+}
---
+++ TT360-app/src/test/java/org/team/tt360app/Tt360AppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team.tt360app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tt360AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
